de2d65df8edd286a94dcbb698fc6988d24a5395d
Peter Maltby <contact@petermaltby.com>
2023-04-28 15:50:52 +0100

    init commit with cent-os script

---
+++ cent-os.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# cent-os.sh
+# author: peterm
+# created: 2023-04-28
+#############################################################
+
+# update and install git and vim
+dnf update -y
+dnf install git vim -y
+git -v
+
+# create admin user
+adduser -m -G wheel peterm
+passwd peterm
+sudo -u peterm mkdir -p --mode=700 /home/peterm/.ssh
+mv /root/.ssh/authorized_keys /home/peterm/.ssh/.
+chown peterm:peterm /home/peterm/.ssh/authorized_keys
+
+# disable root login
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+
+
+cd /home/peterm/ || exit
+sudo -u peterm mkdir gitrepos
+cd gitrepos/ || exit
+
+# bootstrap dotfiles repo
+sudo -u peterm git clone https://github.com/PeterMaltby/.dotfiles.git
+cd .dotfiles || exit
+sudo -u peterm ./bootstrap.sh
+
+# restart ssh daemon
+systemctl reload sshd
+
+echo "your server is ready!"
