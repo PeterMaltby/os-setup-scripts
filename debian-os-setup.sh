d5358a506e740d29dc9ee05a528dcd81be28a516
Peter Maltby <contact@petermaltby.com>
2024-03-20 18:06:02 +0000

    debian

--- debian-os-setup.sh
+++ debian-os-setup.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-# cent-os.sh
+# debian-os.sh
 # author: peterm
 # created: 2023-04-28
 #############################################################
 
 # update and install git and vim
-dnf update -y
-dnf install git vim -y
+curl -sSL https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info | tic -x -
+
+apt update
+apt upgrade -y
+apt install git -y
 git -v
 
 # create admin user
-adduser -m -G wheel peterm
+useradd -m -G sudo -s /bin/bash peterm
 passwd peterm
 sudo -u peterm mkdir -p --mode=700 /home/peterm/.ssh
-mv /root/.ssh/authorized_keys /home/peterm/.ssh/.
+cp /root/.ssh/authorized_keys /home/peterm/.ssh/.
 chown peterm:peterm /home/peterm/.ssh/authorized_keys
 
 # disable root login and password login
-sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+# sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+# sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 
 cd /home/peterm/ || exit
 sudo -u peterm mkdir gitrepos
